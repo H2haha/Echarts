5d7b8c2ee22d5ae99bcddce0907a7aafed625930
rennana <973187407@qq.com>
2017-11-30 16:51:04 +0800

    first commit

---
+++ text.php
@@ -0,0 +1,14 @@
+<?php
+
+	$data = [
+		array('mc' => "衬衫", 'xl' => 10),
+		array('mc' => "羊毛衫", 'xl' => 10),
+		array('mc' => "雪纺衫", 'xl' => 3),
+		array('mc' => "裤子", 'xl' => 57),
+		array('mc' => "高跟鞋", 'xl' => 12),
+		array('mc' => "semir", 'xl' => 89),
+		array('title' => 'Echarts入门'),
+	];
+
+	//echo json_encode($data,true);
+	echo json_encode($data, true);
